0738e6a281501b50da41e14f44cd033052ca7f47
Conchy <xConchyx@gmail.com>
2024-06-21 09:21:23 +0200

    weather done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Weather from './components/Weather.vue'
+
+import Calculator from './components/Calculator.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<nav>
+<div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <Weather />
+  
+</div> 
+</nav>
+
+  <header>
+    <div>
+      <Calculator />
     </div>
+    
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <footer>
+
+  </footer>
+    
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style lang="scss">
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/__tests__/Calculator.spec.js
@@ -0,0 +1,6 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Calculator from '../Calculator.vue';
+
+
+
